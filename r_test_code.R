3b1c67d73735a7cc5c6ac3d47b8e50e2de3075ed
Harith Bahren <harithbahren@gmail.com>
2021-07-16 16:04:14 +0800

    Third commit with addtional print call to check functionality of PAT

--- r_test_code.R
+++ r_test_code.R
@@ -3,3 +3,5 @@ print("This file was created within RStudio")
 print("And it now lives on GitHub")
 
 print("Checking if this updates on GitHub")
+
+print("Checking to see if pushing through PAT works")
